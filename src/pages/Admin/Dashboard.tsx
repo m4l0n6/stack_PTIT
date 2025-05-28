c15b41ea5fcef913689ccf02db0b9c97064c3019
Long Ma <mahonglonghg123@gmail.com>
2025-05-28 18:03:42 +0700

    Edit dashboard

--- src/pages/Admin/Dashboard.tsx
+++ src/pages/Admin/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard: React.FC = () => {
         <div className="p-4">
             <h1 className="mb-4 font-bold text-2xl">Bảng điều khiển</h1>
             <p>Chào mừng đến với bảng điều khiển quản trị viên. Tại đây, bạn có thể quản lý người dùng, câu hỏi và các cài đặt khác.</p>
-            
+    
         </div>
     );
 }
